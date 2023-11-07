a1560934c9c0567060d0f4d6bfb83c6338425be0
Nirmalya Mondal <35608135+nirmalyax@users.noreply.github.com>
2023-11-07 10:58:50 +0530

    Designs

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React from "react";
+import "./Homestyle.css"
 
 export default function () {
   return (
-    <div>
-        <video id='back_video' src="src/assets/37b070b4.mp4" 
-        type="video/mp4" autoPlay loop muted className='video' style={{
-            width: "100%",
-            height: "100%"
-        }}/>
-    </div>
-  )
+    <>
+      <video
+        id="back_video"
+        src="src/assets/37b070b4.mp4"
+        type="video/mp4"
+        autoPlay
+        loop
+        muted
+        className="video"
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "90%",
+          zIndex: "0",
+        }}
+      />
+      <div className="body" style={{
+      }}>
+       <div className="left">
+        <h1>aNythiNg</h1>
+        <h1>wOrth hAving</h1>
+        <h1>TakEs &nbsp; thiS</h1>
+       </div>
+      </div>
+    </>
+  );
 }
